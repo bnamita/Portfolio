48b117a8e5929d652bc8cd460610389e7cdf23b6
Namita Bhatawdekar <nbhatawdekar@mediaocean.com>
2016-11-09 10:23:01 +0530

    Initial commit

---
+++ js/list.js
@@ -0,0 +1,50 @@
+var list = [
+    {
+        "id":"1",
+        "name":"IndianBudgetZoomable",
+        "url":"2016/03/where-does-money-come-from.html",
+        title: "Indian Budget",
+        tech: "d3.js",
+        desc: "This visualization shows the different sources of income for Indian Government for 2016-17 and 2015-16"
+    },
+    {
+        "id":"2",
+        "name":"IndianExpenditureZoomable",
+        "url": "2016/02/where-does-money-go.html",
+        title: "Indian Expenditure",
+        tech: "d3.js",
+        desc: "This visualization shows how Indian Government spent money in various sectors in 2016-17 and 2015-16"
+    },
+    {
+        "id":"3",
+        "name":"IndianExpenditureByMinistry",
+        "url":"2016/02/expenditure-by-ministry.html"  ,
+        title: "India's Expenditure By Ministry",
+        tech: "Javascript, Google Visualization API",
+        desc: "This chart compares India's budget for top 15 ministries for 2016-17 and 2015-16"
+    },
+    {
+        "id":"4",
+        "name":"IndianExpenditureByMinistryComparison",
+        "url": "2016/02/indias-expenditure-by-ministry.html",
+        title: "Expenditure by Ministry - YoY comparison",
+        tech: "Javascript, Google Visualization API",
+        desc: "This chart compares the budget for top 15 ministries for 2016-17 and 2015-16"
+    },
+    {
+        "id":"5",
+        "name":"Hierarchical-CSV-browser",
+        "url":"2016/03/where-does-money-come-from.html" ,
+        title: "Hierarchical CSV Browser - Nagpur Budget",
+        tech: "d3.js",
+        desc: "This visualization reads Nagpur's (a city in India) budget from a csv containing hierarchical data and converts it into a friendly browser"
+    },
+    {
+        "id":"6",
+        "name":"Village_Mapping_v2",
+        "url":"2016/03/where-does-money-come-from.html" ,
+        title: "Maharashtra District Census Data Mapping",
+        tech: "Mapbox, leaflet.js",
+        desc: "A visualization to view census data for Maharashtra (a state in India) for 2011. Variables are viewed in district-wise choropleths"
+    }
+];
---
+++ js/main.js
@@ -0,0 +1,78 @@
+render(list);
+function render(viz_list) {
+    if (!viz_list.length) return;
+
+
+    var viz = d3.select(".projects").selectAll(".viz")
+        .data(viz_list, function(d) { return d.id; })
+        .enter().insert("li", "br")
+        .attr("class", "viz caption")
+        //.text(function(d) { return d.title; })
+        .append("a")
+        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
+
+        .append("img")
+        .attr('title', function(d) {
+            return d.title;
+        })
+        .attr("src", function(d) {
+            return "css/images/" + d.name + ".png"
+        })
+        .attr("class", "viz viz--thumbnail")
+        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
+        .attr("target", "_blank")
+
+
+        //.style("background-image", function(d) { return "url(css/images/" + d.name + ".png)"; })
+        //.append("span")
+        //.attr("class", 'viz_name')
+        //.text(function(d) { return d.title; })
+
+        //.enter().insert("a", "br")
+        //.text(function(d) {
+        //    return d.title;
+        //});
+    d3.select(".projects").selectAll(".viz")
+        .append("div")
+        .attr("class", "caption__overlay")
+        .append("p")
+        .attr("class", "caption__overlay__content")
+        .html(function(d) {
+            return d.desc + ' <br><br><a style="text-decoration: underline;" target="_blank" href="//bnamita.github.io/' + d.name + '"> View Interactive Visualization</a>';
+        });
+
+    d3.select(".projects").selectAll(".viz")
+        .append("span")
+        .attr("class", "viz-title")
+        .text(function(d) { return d.title; })
+        .append("br");
+
+    d3.select(".projects").selectAll(".viz")
+        .append("span")
+        .attr("class", "viz-tech")
+        .html(function(d) { return  d.tech; });
+}
+
+function render_single(viz_list, elem) {
+    if (!viz_list.length) return;
+
+
+    var viz = d3.select("." + elem).selectAll(".viz")
+        .data(viz_list, function(d) { return d.id; })
+        .enter().insert("a", "br")
+        .attr("class", "viz viz--thumbnail--large")
+        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
+        .attr("target", "_blank")
+        .style("background-image", function(d) { return "url(css/images/" + d.name + ".png)"; });
+
+    //viz.append("div")
+    //    .attr("class", "viz-title")
+    //    .text(function(d) { return d.title; });
+}
+
+$(document).ready(function(){
+    $("ul li a").on('mouseover', function() {
+        //$(".active").removeClass('active');
+        //$(this).attr('border-bottom','1px solid red');
+    })
+})
