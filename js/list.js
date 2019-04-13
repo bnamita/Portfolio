3c79690d82d18059fddcb3a7d4cd1690bb9e1955
Namita Bhatawdekar <bnamita@users.noreply.github.com>
2019-04-13 11:58:01 +0530

    Added OWD App

--- js/list.js
+++ js/list.js
@@ -1,14 +1,32 @@
 var list = [
+    
     {
         "id":"1",
+        "name":"OpenWaterDataApp",
+        "url":"2016/03/where-does-money-come-from.html" ,
+        title: "Open Water Data Portal",
+        tech: "Node.js, React.js, Google Maps Javascript API, Google Earth Engine",
+        desc: "A geospatial visualization to Precipitation Data for watersheds in India"
+    },
+    {
+        "id":"2",
+        "name":"Village_Mapping_v2",
+        "url":"2016/03/where-does-money-come-from.html" ,
+        title: "Maharashtra District Census Data Mapping",
+        tech: "Mapbox, leaflet.js",
+        desc: "A visualization to view census data for Maharashtra (a state in India) for 2011. Variables are viewed in district-wise choropleths"
+    },
+    {
+        "id":"3",
         "name":"IndianBudgetZoomable",
         "url":"2016/03/where-does-money-come-from.html",
         title: "Indian Budget",
         tech: "d3.js",
         desc: "This visualization shows the different sources of income for Indian Government for 2016-17 and 2015-16"
     },
+   
     {
-        "id":"2",
+        "id":"4",
         "name":"IndianExpenditureZoomable",
         "url": "2016/02/where-does-money-go.html",
         title: "Indian Expenditure",
@@ -16,7 +34,7 @@ var list = [
         desc: "This visualization shows how Indian Government spent money in various sectors in 2016-17 and 2015-16"
     },
     {
-        "id":"3",
+        "id":"5",
         "name":"IndianExpenditureByMinistry",
         "url":"2016/02/expenditure-by-ministry.html"  ,
         title: "India's Expenditure By Ministry",
@@ -24,7 +42,7 @@ var list = [
         desc: "This chart compares India's budget for top 15 ministries for 2016-17 and 2015-16"
     },
     {
-        "id":"4",
+        "id":"6",
         "name":"IndianExpenditureByMinistryComparison",
         "url": "2016/02/indias-expenditure-by-ministry.html",
         title: "Expenditure by Ministry - YoY comparison",
@@ -32,19 +50,12 @@ var list = [
         desc: "This chart compares the budget for top 15 ministries for 2016-17 and 2015-16"
     },
     {
-        "id":"5",
+        "id":"7",
         "name":"Hierarchical-CSV-browser",
         "url":"2016/03/where-does-money-come-from.html" ,
         title: "Hierarchical CSV Browser - Nagpur Budget",
         tech: "d3.js",
         desc: "This visualization reads Nagpur's (a city in India) budget from a csv containing hierarchical data and converts it into a friendly browser"
-    },
-    {
-        "id":"6",
-        "name":"Village_Mapping_v2",
-        "url":"2016/03/where-does-money-come-from.html" ,
-        title: "Maharashtra District Census Data Mapping",
-        tech: "Mapbox, leaflet.js",
-        desc: "A visualization to view census data for Maharashtra (a state in India) for 2011. Variables are viewed in district-wise choropleths"
     }
+    
 ];
