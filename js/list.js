149c797539d5bbede7c6ecdd7d3b6e7b47f7fe11
bnamita <bnamita@gmail.com>
2019-04-13 12:39:40 +0530

    Added Open water data

--- js/list.js
+++ js/list.js
@@ -1,61 +1,67 @@
 var list = [
-    
-    {
-        "id":"1",
-        "name":"OpenWaterDataApp",
-        "url":"2016/03/where-does-money-come-from.html" ,
-        title: "Open Water Data Portal",
-        tech: "Node.js, React.js, Google Maps Javascript API, Google Earth Engine",
-        desc: "A geospatial visualization to Precipitation Data for watersheds in India"
-    },
-    {
-        "id":"2",
-        "name":"Village_Mapping_v2",
-        "url":"2016/03/where-does-money-come-from.html" ,
-        title: "Maharashtra District Census Data Mapping",
-        tech: "Mapbox, leaflet.js",
-        desc: "A visualization to view census data for Maharashtra (a state in India) for 2011. Variables are viewed in district-wise choropleths"
-    },
-    {
-        "id":"3",
-        "name":"IndianBudgetZoomable",
-        "url":"2016/03/where-does-money-come-from.html",
-        title: "Indian Budget",
-        tech: "d3.js",
-        desc: "This visualization shows the different sources of income for Indian Government for 2016-17 and 2015-16"
-    },
-   
-    {
-        "id":"4",
-        "name":"IndianExpenditureZoomable",
-        "url": "2016/02/where-does-money-go.html",
-        title: "Indian Expenditure",
-        tech: "d3.js",
-        desc: "This visualization shows how Indian Government spent money in various sectors in 2016-17 and 2015-16"
-    },
-    {
-        "id":"5",
-        "name":"IndianExpenditureByMinistry",
-        "url":"2016/02/expenditure-by-ministry.html"  ,
-        title: "India's Expenditure By Ministry",
-        tech: "Javascript, Google Visualization API",
-        desc: "This chart compares India's budget for top 15 ministries for 2016-17 and 2015-16"
-    },
-    {
-        "id":"6",
-        "name":"IndianExpenditureByMinistryComparison",
-        "url": "2016/02/indias-expenditure-by-ministry.html",
-        title: "Expenditure by Ministry - YoY comparison",
-        tech: "Javascript, Google Visualization API",
-        desc: "This chart compares the budget for top 15 ministries for 2016-17 and 2015-16"
-    },
-    {
-        "id":"7",
-        "name":"Hierarchical-CSV-browser",
-        "url":"2016/03/where-does-money-come-from.html" ,
-        title: "Hierarchical CSV Browser - Nagpur Budget",
-        tech: "d3.js",
-        desc: "This visualization reads Nagpur's (a city in India) budget from a csv containing hierarchical data and converts it into a friendly browser"
-    }
-    
+  {
+    id: '1',
+    name: 'OpenWaterDataApp',
+    url: 'https://water-data-web-app.appspot.com/',
+    title: 'Open Water Data Portal',
+    tech: 'Node.js, React.js, Google Maps Javascript API, Google Earth Engine',
+    desc:
+      'A geospatial visualization to Precipitation Data for watersheds in India'
+  },
+
+  {
+    id: '2',
+    name: 'IndianBudgetZoomable',
+    url: '2016/03/where-does-money-come-from.html',
+    title: 'Indian Budget',
+    tech: 'd3.js',
+    desc:
+      'This visualization shows the different sources of income for Indian Government for 2016-17 and 2015-16'
+  },
+
+  {
+    id: '3',
+    name: 'IndianExpenditureZoomable',
+    url: '2016/02/where-does-money-go.html',
+    title: 'Indian Expenditure',
+    tech: 'd3.js',
+    desc:
+      'This visualization shows how Indian Government spent money in various sectors in 2016-17 and 2015-16'
+  },
+  {
+    id: '4',
+    name: 'IndianExpenditureByMinistry',
+    url: '2016/02/expenditure-by-ministry.html',
+    title: "India's Expenditure By Ministry",
+    tech: 'Javascript, Google Visualization API',
+    desc:
+      "This chart compares India's budget for top 15 ministries for 2016-17 and 2015-16"
+  },
+  {
+    id: '5',
+    name: 'IndianExpenditureByMinistryComparison',
+    url: '2016/02/indias-expenditure-by-ministry.html',
+    title: 'Expenditure by Ministry - YoY comparison',
+    tech: 'Javascript, Google Visualization API',
+    desc:
+      'This chart compares the budget for top 15 ministries for 2016-17 and 2015-16'
+  },
+  {
+    id: '6',
+    name: 'Hierarchical-CSV-browser',
+    url: '2016/03/where-does-money-come-from.html',
+    title: 'Hierarchical CSV Browser - Nagpur Budget',
+    tech: 'd3.js',
+    desc:
+      "This visualization reads Nagpur's (a city in India) budget from a csv containing hierarchical data and converts it into a friendly browser"
+  },
+  {
+    id: '7',
+    name: 'Village_Mapping_v2',
+    url: '2016/03/where-does-money-come-from.html',
+    title: 'Maharashtra District Census Data Mapping',
+    tech: 'Mapbox, leaflet.js',
+    desc:
+      'A visualization to view census data for Maharashtra (a state in India) for 2011. Variables are viewed in district-wise choropleths'
+  }
 ];
--- js/main.js
+++ js/main.js
@@ -1,78 +1,121 @@
 render(list);
 function render(viz_list) {
-    if (!viz_list.length) return;
+  if (!viz_list.length) return;
 
+  var viz = d3
+    .select('.projects')
+    .selectAll('.viz')
+    .data(viz_list, function(d) {
+      return d.id;
+    })
+    .enter()
+    .insert('li', 'br')
+    .attr('class', 'viz caption')
+    //.text(function(d) { return d.title; })
+    .append('a')
+    .attr('href', function(d) {
+      var url = '//bnamita.github.io/' + d.name; //blog links
+      if (d.name === 'OpenWaterDataApp') {
+        url = 'https://water-data-web-app.appspot.com/';
+      }
+      return url;
+    })
+    .append('img')
+    .attr('title', function(d) {
+      return d.title;
+    })
+    .attr('src', function(d) {
+      return 'css/images/' + d.name + '.png';
+    })
+    .attr('class', 'viz viz--thumbnail')
+    .attr('href', function(d) {
+      //return '//bnamita.github.io/' + d.name;
+      var url = '//bnamita.github.io/' + d.name; //blog links
+      if (d.name === 'OpenWaterDataApp') {
+        url = 'https://water-data-web-app.appspot.com/';
+      }
+      return url;
+    })
+    .attr('target', '_blank');
 
-    var viz = d3.select(".projects").selectAll(".viz")
-        .data(viz_list, function(d) { return d.id; })
-        .enter().insert("li", "br")
-        .attr("class", "viz caption")
-        //.text(function(d) { return d.title; })
-        .append("a")
-        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
+  //.style("background-image", function(d) { return "url(css/images/" + d.name + ".png)"; })
+  //.append("span")
+  //.attr("class", 'viz_name')
+  //.text(function(d) { return d.title; })
 
-        .append("img")
-        .attr('title', function(d) {
-            return d.title;
-        })
-        .attr("src", function(d) {
-            return "css/images/" + d.name + ".png"
-        })
-        .attr("class", "viz viz--thumbnail")
-        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
-        .attr("target", "_blank")
+  //.enter().insert("a", "br")
+  //.text(function(d) {
+  //    return d.title;
+  //});
+  d3
+    .select('.projects')
+    .selectAll('.viz')
+    .append('div')
+    .attr('class', 'caption__overlay')
+    .append('p')
+    .attr('class', 'caption__overlay__content')
+    .html(function(d) {
+      var url = '//bnamita.github.io/' + d.name;
+      if (d.name === 'OpenWaterDataApp') {
+        url = 'https://water-data-web-app.appspot.com/';
+      }
 
+      return (
+        d.desc +
+        ' <br><br><a style="text-decoration: underline;" target="_blank" href="' +
+        url +
+        '"> View Interactive Visualization</a>'
+      );
+    });
 
-        //.style("background-image", function(d) { return "url(css/images/" + d.name + ".png)"; })
-        //.append("span")
-        //.attr("class", 'viz_name')
-        //.text(function(d) { return d.title; })
+  d3
+    .select('.projects')
+    .selectAll('.viz')
+    .append('span')
+    .attr('class', 'viz-title')
+    .text(function(d) {
+      return d.title;
+    })
+    .append('br');
 
-        //.enter().insert("a", "br")
-        //.text(function(d) {
-        //    return d.title;
-        //});
-    d3.select(".projects").selectAll(".viz")
-        .append("div")
-        .attr("class", "caption__overlay")
-        .append("p")
-        .attr("class", "caption__overlay__content")
-        .html(function(d) {
-            return d.desc + ' <br><br><a style="text-decoration: underline;" target="_blank" href="//bnamita.github.io/' + d.name + '"> View Interactive Visualization</a>';
-        });
-
-    d3.select(".projects").selectAll(".viz")
-        .append("span")
-        .attr("class", "viz-title")
-        .text(function(d) { return d.title; })
-        .append("br");
-
-    d3.select(".projects").selectAll(".viz")
-        .append("span")
-        .attr("class", "viz-tech")
-        .html(function(d) { return  d.tech; });
+  d3
+    .select('.projects')
+    .selectAll('.viz')
+    .append('span')
+    .attr('class', 'viz-tech')
+    .html(function(d) {
+      return d.tech;
+    });
 }
 
 function render_single(viz_list, elem) {
-    if (!viz_list.length) return;
+  if (!viz_list.length) return;
 
+  var viz = d3
+    .select('.' + elem)
+    .selectAll('.viz')
+    .data(viz_list, function(d) {
+      return d.id;
+    })
+    .enter()
+    .insert('a', 'br')
+    .attr('class', 'viz viz--thumbnail--large')
+    .attr('href', function(d) {
+      return '//bnamita.github.io/' + d.name;
+    }) //blog links
+    .attr('target', '_blank')
+    .style('background-image', function(d) {
+      return 'url(css/images/' + d.name + '.png)';
+    });
 
-    var viz = d3.select("." + elem).selectAll(".viz")
-        .data(viz_list, function(d) { return d.id; })
-        .enter().insert("a", "br")
-        .attr("class", "viz viz--thumbnail--large")
-        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
-        .attr("target", "_blank")
-        .style("background-image", function(d) { return "url(css/images/" + d.name + ".png)"; });
-
-    //viz.append("div")
-    //    .attr("class", "viz-title")
-    //    .text(function(d) { return d.title; });
+  //viz.append("div")
+  //    .attr("class", "viz-title")
+  //    .text(function(d) { return d.title; });
 }
 
-$(document).ready(function(){
-    $("ul li a").on('mouseover', function() {
-        //$(".active").removeClass('active');
-        //$(this).attr('border-bottom','1px solid red');
-    })
-})
+$(document).ready(function() {
+  $('ul li a').on('mouseover', function() {
+    //$(".active").removeClass('active');
+    //$(this).attr('border-bottom','1px solid red');
+  });
+});
